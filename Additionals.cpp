af1af05efef2383ea2b11e53ac95fe4bcb24350a
Abdelrahman <abdelrahman.y.salah@gmail.com>
2024-08-15 22:02:14 +0300

    add additionals.cpp

---
+++ Additionals.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Dish.h"
+#include <string>
+using namespace std;
+
+Additionals::Additionals(){
+        Dishes[0] = "Ranch Sauce";
+        Dishes[1] = "Ketchup";
+        Dishes[2] = "Soda";
+        prices[0] = 10.5;
+        prices[1] = 7;
+        prices[2] = 15.25;
+        WantedDish="";
+}
