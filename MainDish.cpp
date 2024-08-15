80aa52cd45817eeea0f02c597219340b0c255ae9
Abdelrahman <abdelrahman.y.salah@gmail.com>
2024-08-15 22:00:14 +0300

    add maindish.cpp

---
+++ MainDish.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Dish.h"
+#include <string>
+using namespace std;
+
+MainDish::MainDish(){
+        Dishes[0] = "Mixed Grill";
+        Dishes[1] = "Chicken Broast";
+        Dishes[2] = "Pizza";
+        prices[0] = 100;
+        prices[1] = 150;
+        prices[2] = 70;
+        WantedDish="";
+}
